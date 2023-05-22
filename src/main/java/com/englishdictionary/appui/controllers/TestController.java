91ab3b9ffc83517fa54711e6e8ce6b0febe96985
ThanTranNghiep <thantrannghiep@gmail.com>
2023-05-22 16:50:46 +0700

    push index

--- src/main/java/com/englishdictionary/appui/controllers/TestController.java
+++ src/main/java/com/englishdictionary/appui/controllers/TestController.java
@@ -2,10 +2,16 @@ package com.englishdictionary.appui.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
+@RequestMapping("/")
 public class TestController {
 
+    @GetMapping
+    public String index() {
+        return "index";
+    }
     @GetMapping("/test")
     public String test() {
         return "test";
